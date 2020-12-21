af74ab94ef7a4c2f7a61fade9f42424218c2b6bd
David Dawkins <david.dawkins@gmail.com>
2020-12-21 13:55:42 +0000

    Comments

--- src/Program.fs
+++ src/Program.fs
@@ -1,5 +1,11 @@
 module Sveltish.Program
 
+    //
+    // Sveltish Elmish
+    // The model mutates in Sveltish, so the function signatures are slightly different to Elmish.
+    // This approach isn't necessary, but it could be helpful in that it encourages the view to
+    // dispatch messages that are then processed only in the update function.
+    //
     let makeProgram host init update view =
         let model = init()
 

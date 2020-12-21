2a19c60e4e648ef1f507130ca8b11aaa61b7b7fa
David Dawkins <david.dawkins@gmail.com>
2020-12-21 13:51:54 +0000

    Clean up

--- src/App.fs
+++ src/App.fs
@@ -3,49 +3,33 @@ module App
 open Sveltish
 open Sveltish.Attr
 open Sveltish.DOM
+open Sveltish.Styling
 
-module BulmaStyling =
-    open Styling
-
-    let bulmaStyleSheet = [
-        rule "body" [
-            fontFamily "sans-serif"
-            color "#4a4a4a"
-            fontSize "1em"
-            fontWeight "400"
-            lineHeight "1.5"
-        ]
-
-        rule ".container" [
-            margin "0 auto"
-            position "relative"
-            width "auto"
-            maxWidth "960px"
-        ]
-
-
+let bulmaStyleSheet = [
+    rule "body" [
+        fontFamily "sans-serif"
+        color "#4a4a4a"
+        fontSize "1em"
+        fontWeight "400"
+        lineHeight "1.5"
     ]
 
-module App =
-    open BulmaStyling
+    rule ".container" [
+        margin "0 auto"
+        position "relative"
+        width "auto"
+        maxWidth "960px"
+    ]
+]
 
-    //let count = Stores.makeStore 0
+let init() = Todos.init()
+let update = Todos.update
 
-    let testApp model dispatch : NodeFactory =
-        Styling.style bulmaStyleSheet <| Html.div [
-            className "container"
-            //Html.p [ text "Sveltish is running" ]
-            //Html.p [ text "Counter" ]
-            //Counter.Counter { InitialCounter = 0; Label = "Click Me"; ShowHint = true }
-            Html.h1 [ text "Sveltish Todos" ]
-            Html.div [ Todos.view model dispatch ]
+let app model dispatch =
+    Styling.style bulmaStyleSheet <| Html.div [
+        class' "container"
+        Html.h1 [ text "Sveltish Todos" ]
+        Html.div [ Todos.view model dispatch ]
+    ]
 
-            Html.pre [
-                text """
-
-
-                """
-            ]
-        ]
-
-Sveltish.Program.makeProgram "sveltish-app" Todos.init Todos.update App.testApp
+Sveltish.Program.makeProgram "sveltish-app" init update app

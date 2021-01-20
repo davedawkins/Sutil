109852c056795e3333a474951d3c3c030bf25a99
Alfonso Garcia-Caro <alfonso.garcia-caro@outlook.com>
2021-01-20 20:33:52 +0900

    Use Feliz.Engine

--- src/App/Counter.fs
+++ src/App/Counter.fs
@@ -5,6 +5,23 @@ open Sutil.Bindings
 open Sutil.DOM
 open Sutil.Attr
 
+open Feliz
+
+// Dummy type to avoid problems with overload resolution in HtmlEngine
+type [<Fable.Core.Erase>] NodeAttr = NodeAttr of NodeFactory
+
+let Html =
+    HtmlEngine
+        { new IConverter<NodeFactory, NodeAttr> with
+            override _.CreateEl(tag, nodes) = el tag (unbox nodes)
+            override _.ChildrenToProp(children) = NodeAttr(fragment children)
+            override _.StringToEl(v) = text v
+
+            override _.EmptyEl = failwith "Not Implemented"
+            override _.FloatToEl(v) = failwith "Not Implemented"
+            override _.IntToEl(v) = failwith "Not Implemented"
+            override _.BoolToEl(v) = failwith "Not Implemented" }
+
 let Counter() =
     let count = Store.make 0
     Html.div [
@@ -18,12 +35,12 @@ let Counter() =
             class' "block"
             Html.button [
                 onClick (fun _ -> count <~= (fun n -> n-1)) []
-                text "-"
+                Html.text "-"
             ]
 
             Html.button [
                 onClick (fun _ -> count <~= (fun n -> n+1)) []
-                text "+"
+                Html.text "+"
             ]
         ]
     ]

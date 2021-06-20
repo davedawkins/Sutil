252412f3145b8f6c39cc538a3326c8eb84982816
Angel D. Munoz <scythes.lol@gmail.com>
2021-06-20 13:59:21 -0500

    add comments

--- src/sutil-wc-playground/src/web-component.js
+++ src/sutil-wc-playground/src/web-component.js
@@ -3,70 +3,45 @@ import { iterate, head, empty } from './.fable/fable-library.3.1.9/List.js';
 
 export function customElFactory(defaultValues, viewFn) {
     return class extends HTMLElement {
-
+        // allow property names to be observed attributes
         static get observedAttributes() { return Object.keys(defaultValues); }
-
         constructor() {
             super();
+            // create a shadowRoot and make it the context
             this.attachShadow({ mode: 'open' });
             const ctx = makeContext(this.shadowRoot);
+            // generate a SutilNode build and asDomElement already do this
             this.__sutilNode = asDomElement(build(viewFn(defaultValues), ctx), ctx);
-
+            // implement getter/setters for properties 
             for (const key of Object.keys(defaultValues)) {
                 Object.defineProperty(this, key, {
                     configurable: false,
                     enumerable: true,
                     get: () => this.__getStorePropValue(key),
+                    // use the attribute changedCallback from the observedAttributes
                     set: value => this.attributeChangedCallback(key, this.__getStorePropValue(key), value)
                 });
             }
         }
-
+        attributeChangedCallback(name, oldVal, newVal) {
+            this.__setStorePropValue(name, newVal);
+        }
+        disconnectedCallback() {
+            iterate(item => item?.Dispose(), this.shadowRoot?.firstChild?.__sutil_disposables);
+            iterate(item => item?._dispose(), this.shadowRoot?.firstChild?.__sutil_groups);
+        }
+        // utility methods to get/set values from the store
         get __sutilFistElStore() {
             return head(this.shadowRoot?.firstChild?.__sutil_disposables || empty());
         }
-
         __getStorePropValue(key) {
             const store = this.__sutilFistElStore;
             return store?.Value?.[key];
         }
-
         __setStorePropValue(key, value) {
             const store = this.__sutilFistElStore;
             if (!key || !store) return;
-
             store.Update(store => ({ ...store, [key]: value }));
-        }
-
-        attributeChangedCallback(name, oldVal, newVal) {
-            this.__setStorePropValue(name, newVal);
-        }
-
-        disconnectedCallback() {
-            iterate(item => item?.Dispose(), this.shadowRoot?.firstChild?.__sutil_disposables);
-            iterate(item => item?._dispose(), this.shadowRoot?.firstChild?.__sutil_groups);
-        }
-    };
-}
-
-export function customElFactoryNoProps(viewFn) {
-    return class extends HTMLElement {
-        constructor() {
-            super();
-            this.attachShadow({ mode: 'open' });
-            const attributeNames = new Set(this.getAttributeNames());
-            const props = {};
-            for (const key of attributeNames.values()) {
-                props[key] = this.getAttribute(key);
-            }
-            const sutilNode = viewFn(props);
-            const ctx = makeContext(this.shadowRoot);
-            asDomElement(build(sutilNode, ctx), ctx);
-            // attach the content to the shadow DOM
-        }
-        disconnectedCallback() {
-            iterate(item => item?.Dispose(), this.shadowRoot?.firstChild?.__sutil_disposables);
-            iterate(item => item?._dispose(), this.shadowRoot?.firstChild?.__sutil_groups);
         }
     };
 }
